7683c823099a3b35b1ca9e8e080b91fcd72c9819
Harumiya2005 <johnriel.layao@carsu.edu.ph>
2026-05-25 00:50:29 +0800

    Updated Axios URL for production

--- FRONTEND/src/axios.js
+++ FRONTEND/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://csu-capstone-thesis-archive.onrender.com/api', 
     // <-- Removed the withCredentials line here!
     headers: {
         'Accept': 'application/json',
